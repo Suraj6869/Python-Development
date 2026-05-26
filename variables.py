1679fe7cf2366e017803d2d37a882ccc64e57825
SURAJ6869 <suraj.programs@gmail.com>
2026-05-26 23:16:43 +0530

    This is my first file for learning

---
+++ variables.py
@@ -0,0 +1,18 @@
+"""
+1. variables in python
+  a. declaring variables
+  b. assigning values to variables
+  c. variable naming conventions
+  d. data types of variables
+  e. type conversion of variables
+  f. global and local variables
+  g. constants in python
+  h. variable scope in python
+  i. variable lifetime in python
+  j. variable memory management in python
+  k. variable best practices in python
+  l. variable debugging in python
+  m. variable performance optimization in python
+  n. variable security in python
+  o. variable documentation in python
+"""
